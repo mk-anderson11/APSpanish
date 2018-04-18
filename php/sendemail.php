ed3b5dfd880fe66ecc8cafe444388f09c537bcac
mk-anderson11 <matt.anderson11@gmail.com>
2018-04-18 05:19:58 -0700

    Edit contact Modal to use sendemail.php

---
+++ php/sendemail.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_POST['submit']))
+{
+  $msg = 'Name: ' . $_POST['name'] . "\n"
+      . 'Email: ' . $_POST['email'] . "\n"
+      . 'Message: ' .$_POST['comment'];
+
+  $to = "matt.anderson11@gmail.com";
+  //mail('matt.anderson11@gmail.com', 'Sample Email Subject', $msg);
+
+  $mail = mail($to, "Sample Email Subject", $msg);
+  if($mail)
+  {
+    echo "Thank you for using our mail form";
+  }
+  else {
+    echo "Mail sending failed.";
+  }
+
+  //header('location: ../index.html');
+
+} else {
+  echo "error; you need to submit the form!";
+  //header('location: ../index.html');
+}
+
+
+
+?>
